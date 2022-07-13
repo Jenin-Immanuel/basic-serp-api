0fd33c52fe5953fa373f95c0b854715234c4d145
Jenin <jeninimmi2003@gmail.com>
2022-07-13 17:49:55 +0530

    🏗️ Initialized `FastApi`

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
-from serp import google_search
+from serp_mod import google_search
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return { "info": "Initalized API" }
+
+
+
 
 query = input("Enter the term you want to search on google:")
 results = google_search(query)
